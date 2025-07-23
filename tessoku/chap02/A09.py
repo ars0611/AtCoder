c1f0d7c4888a14bf77b9a2d8308b39a51ed15335
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-07-23 13:39:06 +0900

    鉄則A09

--- tessoku/chap02/A09.py
+++ tessoku/chap02/A09.py
@@ -8,6 +8,27 @@ import bisect
 
 #----------------------------------------#
 h,w,n = map(int, input().split())
-s = [[0]*(w+1) for _ in range(h+1)]
-#差分の累積和を取るとうまくいきそう
+s = [[0]*(w+2) for _ in range(h+2)]
+#差分を入力
 for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    s[a][b] += 1
+    s[a][d+1] -= 1
+    s[c+1][b] -= 1
+    s[c+1][d+1] += 1
+
+#横の累積和をとる
+for i in range(1,h+1):
+    for j in range(1,w+1):
+        s[i][j] += s[i][j-1]
+#縦の累積和をとる
+for j in range(1,w+1):
+    for i in range(1,h+1):
+        s[i][j] += s[i-1][j]
+
+for i in range(1,h+1):
+    for j in range(1,w+1):
+        if j == w:
+            print(s[i][j])
+            break
+        print(s[i][j],end=" ")
