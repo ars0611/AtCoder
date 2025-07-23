b989b878517801cca8d9afaab6b6c55595008bcb
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-07-24 00:01:47 +0900

    ABC399

--- contest/ABC399/a.py
+++ contest/ABC399/a.py
@@ -7,3 +7,11 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+n = int(input())
+s = input()
+t = input()
+cnt = 0
+for i in range(n):
+    if s[i] != t[i]:
+        cnt += 1
+print(cnt)
--- contest/ABC399/b.py
+++ contest/ABC399/b.py
@@ -7,3 +7,13 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+n = int(input())
+p = list(map(int, input().split()))
+
+#人iについて、iの順位はp[i]より高い得点の人数+1
+for i in range(n):
+    rank = 1
+    for j in range(n):
+        if p[i] < p[j]:
+            rank += 1
+    print(rank)
