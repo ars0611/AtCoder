e70b87461f86f2ce91075c85ac1f5fba213d2ab6
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-07-25 00:05:55 +0900

    鉄則A10

--- tessoku/chap02/A10.py
+++ tessoku/chap02/A10.py
@@ -11,6 +11,16 @@ n = int(input())
 a = list(map(int, input().split()))
 d = int(input())
 
+#aについて先頭からと末尾からとで累積MAXをとる
+p = [None]*n
+q = [None]*n
+p[0] = a[0]
+q[0] = a[n-1]
+for i in range(1,n):
+    p[i] = max(a[i], p[i-1])
+    q[i] = max(a[n-1-i], q[i-1])
+
+
 for _ in range(d):
     l, r = map(int, input().split())
-    
+    print(max(p[l-2], q[n-r-1]))
