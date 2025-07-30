797e28b74287e0f94eb3700051581dfd9392e24d
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-07-31 00:16:16 +0900

    鉄則本A20

--- tessoku/chap04/A20.py
+++ tessoku/chap04/A20.py
@@ -9,5 +9,20 @@ from collections import defaultdict
 import itertools
 
 #----------------------------------------#
-s ,t = input()
+s = input()
+t = input()
 
+#DynamicProgramming
+#dp[i][j] := sのi文字目までとtのj文字目までで作れるLCS（最長共通部分列）
+dp = [[0]*(len(t)+1) for _ in range(len(s)+1)]
+
+for i in range(1,len(s)+1):
+    for j in range(len(t)+1):
+        if i >= 1 and j >= 1 and s[i-1] == t[j-1]:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1] + 1)
+        elif i >= 1 and j >= 1:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        elif i >= 1:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[len(s)][len(t)])
