bb875f0f584e0d232a5752f42ffadbc08ad8d311
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-07 22:44:19 +0900

    ABC381D

--- contest/ABC381/d.py
+++ contest/ABC381/d.py
@@ -15,5 +15,37 @@ n = int(input())
 a = list(map(int, input().split()))
 
 #尺取り
-ans = 0
-l = 
+def syakutori(start):
+    counter = Counter()
+    l = r = start
+    cnt = 0
+    res = 0
+    
+    while l < n-1:
+        
+        #左端を決定
+        if a[l] != a[l+1]:
+            l += 2
+            r = l
+            cnt = 0
+            counter.clear()
+        
+        #右端をずらす
+        elif r < n-1 and a[r] == a[r+1] and counter[a[r]] == 0:
+            counter[a[r]] += 1
+            cnt += 2
+            r += 2
+            res = max(res, cnt)
+        
+        #左端をずらす
+        else:
+            counter[a[l]] -= 1
+            l += 2
+            cnt -= 2
+            if l > r:
+                r = l
+        
+    return res
+
+ans = max(syakutori(0), syakutori(1))
+print(ans)
