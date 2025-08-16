b96ebcb4e1edc059d9f0b2943634f25148aca0ec
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-16 10:42:12 +0900

    ABC415 C

--- contest/ABC415/c.py
+++ contest/ABC415/c.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10*6)
+sys.setrecursionlimit(10**6)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
 import math
@@ -7,3 +7,31 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+t = int(input())
+for _ in range(t):
+    
+    #入力
+    n = int(input())
+    s = input()
+    
+    #状態0は安全
+    s = "0" + s
+    dp = [False] * (len(s))
+    dp[0] = True
+    
+    #各状態からいける状態を探索
+    for i in range(len(s)):
+        
+        #危険な状態はスキップ
+        if dp[i] == False:
+            continue
+        
+        for j in range(n):
+            
+            if i & (1 << j) == "1":
+                continue
+
+            if s[i | (1 << j)] == "0":
+                dp[i | (1 << j)] = True
+
+    print("Yes" if dp[-1] else "No")
