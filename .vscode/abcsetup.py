76fbd49a64e5b91c66cba0e6e15b3238be1d4c08
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-21 00:19:42 +0900

    鉄則本A67~

--- .vscode/abcsetup.py
+++ .vscode/abcsetup.py
@@ -6,15 +6,9 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-from collections import Counter
-from collections import deque
-from collections import defaultdict
+from collections import Counter, deque, defaultdict
 from sortedcontainers import SortedSet, SortedList, SortedDict
-import heapq
-import bisect
-import math
-import itertools
-import copy
+import heapq, bisect, math, itertools, copy
 
 #----------------------------------------#
 '''
---
+++ tessoku/chap09/B67.py
@@ -0,0 +1,61 @@
+import sys
+sys.setrecursionlimit(10**7)
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+import heapq, bisect, math, itertools, copy
+
+#----------------------------------------#
+class UnionFind:
+    # n頂点のUnion-Find木を生やす
+    # 親を表す配列とグループのサイズを表す配列を用意する
+    def __init__(self, n):
+        self.par = [-1] * n
+        self.siz = [1] * n
+    
+    # 頂点xの根を求める関数を定義する
+    def root(self, x):
+        # 根にたどり着くまで親をたどる
+        while self.par[x] != -1:
+            x = self.par[x]
+        return x
+
+    # 頂点u,vを統合する関数を定義する
+    def unite(self, u, v):
+        root_u = self.root(u)
+        root_v = self.root(v)
+        
+        # 既に統合されているなら何もしない
+        if root_u == root_v:
+            return
+
+        # 頂点数の多い方に統合する（これにより計算量が抑えられる）
+        if self.siz[root_u] < self.siz[root_v]:
+            self.par[root_u] = root_v
+            self.siz[root_v] += self.siz[root_u]
+        else:
+            self.par[root_v] = root_u
+            self.siz[root_u] += self.siz[root_v]
+
+    # 頂点u,vが同じグループに属するか判定する関数を定義する
+    def same(self, u, v):
+        return self.root(u) == self.root(v)
+    
+# 入力
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+for i in range(m):
+    graph[i][0] -= 1
+    graph[i][1] -= 1
+
+# クラスカル法で最小全域木を求める
+graph.sort(reverse=True, key=lambda x: x[2])  # 辺の長さについてソート
+uf = UnionFind(n)
+ans = 0
+for u, v, c in graph:
+    if not uf.same(u, v):
+        uf.unite(u, v)
+        ans += c
+
+print(ans)
