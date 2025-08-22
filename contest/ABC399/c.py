ea3ca53e5ef4946a5830d1ee964f2794e3381692
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-22 14:59:40 +0900

    ABC399C

--- contest/ABC399/c.py
+++ contest/ABC399/c.py
@@ -7,3 +7,49 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+class UnionFind:
+    # n頂点のUnion-Find木を生やす
+    # 親を表す配列とグループのサイズを表す配列を用意する
+    def __init__(self, n):
+        self.par = [-1] * n
+        self.siz = [1] * n
+    
+    # 頂点xの根を求める関数を定義する
+    def root(self, x):
+        # 根にたどり着くまで親をたどる
+        while self.par[x] != -1:
+            x = self.par[x]
+        return x
+    
+    # 頂点u,vを統合する関数を定義する
+    def unite(self, u, v):
+        root_u = self.root(u)
+        root_v = self.root(v)
+        
+        # 既に統合されているなら何もしない
+        if root_u == root_v:
+            return
+
+        # 頂点数の多い方に統合する（これにより計算量が抑えられる）
+        if self.siz[root_u] < self.siz[root_v]:
+            self.par[root_u] = root_v
+            self.siz[root_v] += self.siz[root_u]
+        else:
+            self.par[root_v] = root_u
+            self.siz[root_u] += self.siz[root_v]
+
+    # 頂点u,vが同じグループに属するか判定する関数を定義する
+    def same(self, u, v):
+        return self.root(u) == self.root(v)
+
+cnt = 0    
+n, m = map(int, input().split())
+uf = UnionFind(n)
+for _ in range(m):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    if not uf.same(a, b):
+        uf.unite(a, b)
+        cnt += 1
+print(m - cnt)
