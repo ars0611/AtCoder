621e63e0f60e3d8e3726a277ce5a38b58463fa85
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-27 09:27:54 +0900

    ABC416D

--- contest/ABC416/d.py
+++ contest/ABC416/d.py
@@ -7,3 +7,23 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = sorted(list(map(int, input().split())))
+    b = sorted(list(map(int, input().split())))
+    a.reverse()
+    cnt = 0
+    i = 0
+    j = 0
+    while i < n and j < n:
+        if a[i] + b[j] < m:
+            j += 1
+        else:
+            cnt += 1
+            i += 1
+            j += 1
+    print(sum(a) + sum(b) - cnt * m)
+
+#a_i + b_i >= m をできるだけ多く作る貪欲で答えが求まるのは考察できてた。
+# 尺取り法頭になさすぎた
