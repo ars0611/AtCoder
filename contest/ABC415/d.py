0e7cf42f25123dd4da9ead0d6d6d1277632ab91e
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-27 14:36:51 +0900

    ABC415D

--- contest/ABC415/d.py
+++ contest/ABC415/d.py
@@ -5,5 +5,22 @@ if len(sys.argv) == 2:
 import math
 from collections import deque
 import bisect
-
+from collections import defaultdict
 #----------------------------------------#
+# 入力
+# 必要瓶数と交換レートで受け取る
+n, m = map(int, input().split())
+exc = []
+for _ in range(m):
+    a_i, b_i = map(int, input().split())
+    exc.append((a_i, a_i - b_i))
+
+exc.sort(key=lambda x: x[1]) # 交換レートについてsort
+cur = n # 手持ちの瓶数
+ans = 0 # 手持ちのシール数
+# 小さい順に見ていく
+for i in range(m):
+    if cur >= exc[i][0]:
+        ans += max((math.ceil(cur - exc[i][0])) // exc[i][1] + 1, 0) # できるかぎり交換
+        cur -= max(((math.ceil(cur - exc[i][0])) // exc[i][1] + 1) * exc[i][1], 0) # 交換した回数分瓶を減らす
+print(ans)
