83489c2fda09a58643c422b9eb4e2d0e3eec0eaa
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-28 19:45:27 +0900

    ABC412C

--- contest/ABC412/c.py
+++ contest/ABC412/c.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10*6)
+sys.setrecursionlimit(10**6)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
 import math
@@ -7,3 +7,22 @@ from collections import deque
 import bisect
 
 #----------------------------------------#
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = list(map(int, input().split()))
+    dmn = [s[0], s[n-1]]
+    cur = s[0]
+    s.sort()
+    ans = 2
+    while 2 * cur < dmn[-1]:
+        idx = bisect.bisect_right(s, 2 * cur)
+        if s[idx - 1] != cur:
+            cur = s[idx - 1]
+            ans += 1
+        else:
+            break
+    if 2 * cur < dmn[-1]:
+        print(-1)
+    else:
+        print(ans)
