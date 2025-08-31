4abb724372aa274bf1c1b9bdfcb842585dadbe11
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-31 09:58:33 +0900

    ABC358D

--- contest/ABC358/d.py
+++ contest/ABC358/d.py
@@ -9,3 +9,20 @@ from more_itertools import distinct_permutations
 import heapq, bisect, math, itertools
 
 #----------------------------------------#
+n, m = map(int, input().split())
+a = deque(sorted(list(map(int, input().split()))))
+b = sorted(list(map(int, input().split())))
+
+i = 0
+ans = 0
+while a and i < m:
+    if a[0] >= b[i]:
+        ans += a.popleft()
+        i += 1
+    else:
+        a.popleft()
+
+if i == m:
+    print(ans)
+else:
+    print(-1)
