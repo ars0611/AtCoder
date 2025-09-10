a4e24b5a73cab05286e7cc8fd82b766c97e0b0d4
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-10 14:27:36 +0900

    ABC414D

--- contest/ABC414/d.py
+++ contest/ABC414/d.py
@@ -1,9 +1,22 @@
 import sys
-sys.setrecursionlimit(10*6)
+sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+n, m = map(int, input().split())
+x = list(map(int, input().split()))
+x.sort()
+
+diff = [x[i] - x[i-1] for i in range(1, n)]
+diff.sort()
+
+ans = 0
+for i in range(n-m):
+    ans += diff[i]
+print(ans)
