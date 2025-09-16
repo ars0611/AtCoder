ea0d45c1922ba065b1375515782d77732372d6bd
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-16 13:18:41 +0900

    ABC349D

--- contest/ABC349/d.py
+++ contest/ABC349/d.py
@@ -9,3 +9,22 @@ from more_itertools import distinct_permutations
 import heapq, bisect, math, itertools
 
 #----------------------------------------#
+l, r = map(int, input().split())
+
+p = [1] #2**iを前計算しとこうかな 
+for i in range(61):
+    p.append(p[-1] * 2)
+
+ans = []
+while l != r:
+    i = 0
+    while l + p[i] <= r and l % p[i] == 0:
+        i += 1
+    ans.append([l, l + p[i - 1]])
+    l = l + p[i - 1]
+
+print(len(ans))
+for i in range(len(ans)):
+    print(*ans[i])
+
+# なんだこの数学問題は。
