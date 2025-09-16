7911c2700141f5c504fdea33015f3bba4bfccd59
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-16 14:09:07 +0900

    ABC356D

--- contest/ABC356/d.py
+++ contest/ABC356/d.py
@@ -9,9 +9,25 @@ from more_itertools import distinct_permutations
 import heapq, bisect, math, itertools
 
 #----------------------------------------#
+#10進数nをb進数変換する関数(2 <= b <= 10)
+def base10_to_baseb(n,b):
+    res = ""
+    while n > 0:
+        res = f'{n % b}' + res
+        n //= b
+    if res == "":
+        return "0"
+    else:
+        return res
+
 n, m = map(int, input().split())
+mod = 998244353 
+m_base_2 = base10_to_baseb(m, 2)
 ans = 0
-for i in range(n):
-    p = i & m
-    ans += (format(p, 'b').count('1')) % 998244353
-print(ans)
+for i in range(len(m_base_2)):
+    if m_base_2[-i-1] == "1":
+        ans += (n+1) // 2**(i + 1) * 2**i
+        if (n+1) % 2**(i + 1) > 2**i:
+            ans += (n+1) % 2**(i + 1) - 2**i
+    ans %= mod
+print(ans % mod)
--- library/base_conversion.py
+++ library/base_conversion.py
@@ -1,10 +1,13 @@
-#10進数nをb進数変換する関数
+#10進数nをb進数変換する関数(2 <= b <= 10)
 def base10_to_baseb(n,b):
     res = ""
     while n > 0:
         res = f'{n % b}' + res
         n //= b
-    return res
+    if res == "":
+        return "0"
+    else:
+        return res
 
 #b進数nを10進数変換する関数(2 <= b <= 9)
 def baseb_to_base10(n, b):
