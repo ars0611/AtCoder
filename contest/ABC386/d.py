7191fb512f5320db6f600fb718a6e598a93af62b
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-19 14:07:02 +0900

    ABC386D

--- contest/ABC386/d.py
+++ contest/ABC386/d.py
@@ -9,3 +9,21 @@ from collections import defaultdict
 import itertools
 
 #----------------------------------------#
+n, m = map(int, input().split())
+painted = []
+for _ in range(m):
+    x, y, c = input().split()
+    x = int(x)
+    y = int(y)
+    painted.append((x,y,c))
+painted.sort()
+min_y = float('inf')
+for x, y, c in painted:
+    if c == "W":
+        min_y = min(min_y, y)
+    else:
+        if y >= min_y:
+            print("No")
+            break
+else:
+    print("Yes")
