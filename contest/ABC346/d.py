eaee5f7e59188f07a91a1eaae0d377a2ebb815a2
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-19 10:25:07 +0900

    ABC346D

--- contest/ABC346/d.py
+++ contest/ABC346/d.py
@@ -10,8 +10,25 @@ import heapq, bisect, math, itertools
 
 #----------------------------------------#
 n = int(input())
-s = input()
+s = input().strip()
 c = list(map(int, input().split()))
 
 #10...10 + 01.. or 01...01 + 10.. or 01...010 + 01... or 10...101 + 10...
-c10 = []
+c10 = [0]
+c01 = [0]
+for i in range(n):
+    if i % 2 == 0 and s[i] == "1" or i % 2 == 1 and s[i] == "0":
+        c01.append(c01[-1] + c[i])
+        c10.append(c10[-1])
+    else:
+        c10.append(c10[-1] + c[i])
+        c01.append(c01[-1])
+
+ans = float('inf')
+for i in range(n - 1):
+    cand = min(c01[i + 1] + c10[n] - c10[i + 1], c10[i + 1] + c01[n] - c01[i + 1])
+    ans = min(ans, cand)
+print(ans)
+
+# 良い文字列は限られてるからそれを普通に作るだけ。
+# ABC422Cと似てる
