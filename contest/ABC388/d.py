57f58115ed2c6c0acb157dade0c968ef3a3b8bcb
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-22 09:23:53 +0900

    ABC388D

--- contest/ABC388/d.py
+++ contest/ABC388/d.py
@@ -9,3 +9,23 @@ from collections import defaultdict
 import itertools
 
 #----------------------------------------#
+n = int(input())
+a = list(map(int, input().split()))
+imos = [0]*(n)
+for i in range(n):
+    if i > 0:
+        imos[i] += imos[i - 1]
+    a[i] += imos[i]
+    if a[i] > 0:
+        if i + 1 < n:
+            imos[i + 1] += 1
+        if n - i > a[i]:
+            if i + a[i] + 1 < n:
+                imos[i + a[i] + 1] -= 1
+            a[i] -= a[i]
+        else:
+            a[i] -= n - i - 1
+
+print(*a)
+
+# いもしながら配ればよいのでは？
