05fbd521ebab0e5e998b9a8b13986bf695024802
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-26 13:58:43 +0900

    ABC391D

--- contest/ABC391/d.py
+++ contest/ABC391/d.py
@@ -2,10 +2,39 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
-from collections import defaultdict
-import itertools
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+n, w = map(int, input().split())
+
+x = [[] for _  in range(w)] # 列ごとに積み上げて管理
+for i in range(n):
+    xi, yi = map(int, input().split())
+    x[xi-1].append((yi, i))
+m = n # 消えるのは列で一番低いとこの高さ分だけ
+for i in range(w):
+    x[i].sort()
+    m = min(m, len(x[i]))
+
+# i行目が何秒後に消えるか
+h = [0]*m
+for i in range(m):
+    for j in range(w):
+        h[i] = max(h[i], x[j][i][0])
+
+block = [float("inf")] * n
+for i in range(len(h)):
+    for j in range(w):
+        block[x[j][i][1]] = h[i]
+
+q = int(input())
+for _ in range(q):
+    t, a = map(int, input().split())
+    a -= 1
+    print("Yes" if t < block[a] else "No")
+
+# 座圧ちっくに考えた。もうちょいきれいにコード書けた気もするが、リファクタはいいや
