aa3a0481e0bb9ec036fda300a95b2fc61110630d
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-26 15:02:36 +0900

    ABC392D

--- contest/ABC392/d.py
+++ contest/ABC392/d.py
@@ -2,11 +2,25 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
-from collections import defaultdict
-import itertools
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
-
+n = int(input())
+dice= []
+side = []
+for _ in range(n):
+    k, *a = map(int, input().split())
+    dice.append(Counter(a))
+    side.append(k)
+ans = 0
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        prob = 0
+        for k in dice[i].keys():
+            prob += dice[i][k] / side[i] * dice[j][k] / side[j]
+        ans = max(ans, prob)
+print(ans)
