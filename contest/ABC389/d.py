d39373cb5793086172b6eeb8850ddcafe20f68dd
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-26 12:51:11 +0900

    ABC389D

--- contest/ABC389/d.py
+++ contest/ABC389/d.py
@@ -2,10 +2,23 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
-from collections import defaultdict
-import itertools
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+r = int(input())
+
+def ok(i, j):
+    return (2 * i + 1)**2 + (2 * j + 1)**2 <= 4 * r**2
+cnt = 0
+m = r - 1
+for i in range(1, r):
+    while not ok(i, m):
+        m -= 1
+    cnt += m
+ans = (r - 1) * 4 + 1
+ans += 4 * cnt
+print(ans)
