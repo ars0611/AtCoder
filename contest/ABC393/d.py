89b3fcfedf49588d5d81de9221e15772567b94d3
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-29 10:09:46 +0900

    ABC393 D

--- contest/ABC393/d.py
+++ contest/ABC393/d.py
@@ -2,10 +2,26 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
-from collections import defaultdict
-import itertools
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+n = int(input())
+s = input()
+c1 = 0
+for ch in s:
+    if ch == "1":
+        c1 += 1
+now = 0
+ans = 0
+for ch in s:
+    if ch == "0":
+        ans += min(now, c1 - now)
+    else:
+        now += 1
+print(ans)
+# 1をかたまりで移動させる感覚
+# nowは1が登場した回数を表す
