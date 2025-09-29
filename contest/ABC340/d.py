436b040f7fe36fbf4ae90b03aeae7727f5b078ab
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-29 11:27:10 +0900

    ABC340D

--- contest/ABC340/d.py
+++ contest/ABC340/d.py
@@ -10,3 +10,25 @@ import heapq, bisect, math, itertools
 
 #----------------------------------------#
 n = int(input())
+graph = [[] for _ in range(n)]
+for i in range(n-1):
+    a, b, x = map(int, input().split())
+    x -= 1
+    graph[i].append((a, i+1))
+    graph[i].append((b, x))
+
+visited = [False]*n
+pq = graph[0][:]
+heapq.heapify(pq)
+visited[0] = True
+while pq:
+    cost, stage = heapq.heappop(pq)
+    if visited[stage] == True:
+        continue
+    visited[stage] = True
+    if stage == n - 1:
+        print(cost)
+        break
+    for nxtcost, nxtstage in graph[stage]:
+        if not visited[nxtstage]:
+            heapq.heappush(pq, (cost+nxtcost, nxtstage))
