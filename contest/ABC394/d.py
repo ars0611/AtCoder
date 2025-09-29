f701faa63031855a1d67411aff62f04614755783
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-29 13:06:53 +0900

    ABC394D

--- contest/ABC394/d.py
+++ contest/ABC394/d.py
@@ -2,10 +2,21 @@ import sys
 sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
-from collections import defaultdict
-import itertools
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+s = input().strip()
+stack = []
+m = set([("(", ")"), ("<", ">"), ("[", "]")])
+for i in range(len(s)):
+    stack.append(s[i])
+    if len(stack) >= 2:
+        if (stack[-2], stack[-1]) in m:
+            stack.pop()
+            stack.pop()
+    
+print("Yes" if not stack else "No")
