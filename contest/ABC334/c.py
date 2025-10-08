8c4fe3ddfa77459a887e73997b32a27bfe114952
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-08 09:10:26 +0900

    ABC334C

--- contest/ABC334/c.py
+++ contest/ABC334/c.py
@@ -11,7 +11,24 @@ import heapq, bisect, math, itertools
 #----------------------------------------#
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
-
+pres = [0]
+sufs = [0]
+if k % 2 == 0:
+    ans = 0
+    for i in range(0, k, 2):
+        ans += a[i + 1] - a[i]
+else:
+    pres = [0] # 先頭からペアをつくったときの累積和
+    sufs = [0] # 末尾からペアを作った時の累積和
+    for i in range(0, k - 1, 2):
+        pres.append(pres[-1] + a[i + 1] - a[i])
+    for i in range(k - 1, 1, -2):
+        sufs.append(sufs[-1] + a[i] - a[i - 1])
+    sufs.reverse()
+    ans = float("inf")
+    for i in range(len(sufs)):
+        ans = min(ans, sufs[i] + pres[i])
+print(ans)
 # お隣同士をペアにすればうまくいきそうなのは、直観的にそうだった。
 # 選ばないやつを決めたとき,ペアが一個ずれるせいでこの問題は難しくなってる
 # 末尾からの累積和を取っておくことで解決 <-天才か？？
