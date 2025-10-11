2d8f13d32bf978367300045823c940bef514c0b0
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-11 19:56:48 +0900

    ABC344D

--- contest/ABC344/d.py
+++ contest/ABC344/d.py
@@ -11,5 +11,16 @@ import heapq, bisect, math, itertools
 #----------------------------------------#
 t = input().strip()
 n = int(input())
-
-# dp解説ACすらできなかったのでこれはTodoなり
+dp = [[float("inf")] * (len(t) + 1) for _ in range(n + 1)]
+dp[0][0] = 0
+for i in range(1, n + 1):
+    bag = list(input().split())
+    a = int(bag[0])
+    for j in range(a):
+        s = bag[1 + j].strip()
+        for k in range(len(t) + 1):
+            if k + len(s) <= len(t) and t[:k] + s == t[:k+len(s)]:
+                dp[i][k + len(s)] = min(dp[i - 1][k + len(s)], dp[i - 1][k] + 1, dp[i][k + len(s)])
+            dp[i][k] = min(dp[i - 1][k], dp[i][k])
+print(dp[n][len(t)] if dp[n][len(t)] != float("inf") else -1)
+print(dp)
