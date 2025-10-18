e3b83ae13a072d74256351cb4993404cd1e5a9f5
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-18 20:27:04 +0900

    ABC326

---
+++ contest/ABC326/b.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(10**7)
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
+
+#----------------------------------------#
+n = int(input())
+for i in range(n, 1000):
+    s = str(i)
+    if int(s[0]) * int(s[1]) == int(s[2]):
+        print(i)
+        break
---
+++ contest/ABC326/c.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10**7)
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
+
+#----------------------------------------#
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+ans = 0
+for i in range(n):
+    idx = bisect.bisect_left(a, a[i] + m)
+    cnt = idx - i
+    ans = max(ans, cnt)
+print(ans)
