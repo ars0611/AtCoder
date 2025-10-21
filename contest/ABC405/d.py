4f8e47ad4d675498950962e4a6a68e4ad716ea47
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-21 12:50:35 +0900

    ABC405D

--- contest/ABC405/d.py
+++ contest/ABC405/d.py
@@ -1,9 +1,40 @@
 import sys
-sys.setrecursionlimit(10*6)
+sys.setrecursionlimit(10**7)
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
-import math
-from collections import deque
-import bisect
+input = sys.stdin.readline
+from collections import Counter, deque, defaultdict
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from more_itertools import distinct_permutations
+import heapq, bisect, math, itertools
 
 #----------------------------------------#
+h, w = map(int, input().split())
+grid = [list(input().strip()) for _ in range(h)]
+ans = [grid[i][:] for i in range(h)]
+
+direction = {0:"^", 1:"v", 2:">", 3:"<"}
+q = deque()
+for i in range(h):
+    for j in range(w):
+        if grid[i][j] == "E":
+            q.append((i, j, -1))
+
+di = [-1, 1, 0, 0]
+dj = [0, 0, 1, -1]
+while q:
+    curi, curj, d = q.popleft()
+    if ans[curi][curj] != "." and ans[curi][curj] != "E": continue
+    if ans[curi][curj] == ".":
+        ans[curi][curj] = direction[d // 2 * 2 + (d + 1) % 2]
+    for k in range(4):
+        ni = curi + di[k]
+        nj = curj + dj[k]
+        if 0 <= ni < h and 0 <= nj < w and ans[ni][nj] == ".":
+            q.append((ni, nj, k))
+
+for i in range(h):
+    s = ""
+    for j in range(w):
+        s += ans[i][j]
+    print(s)
