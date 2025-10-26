7e5a46fa9561980632fd938428e64b3aee53b780
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-27 01:00:05 +0900

    ABC429E

--- contest/ABC429/e.py
+++ contest/ABC429/e.py
@@ -9,17 +9,6 @@ from more_itertools import distinct_permutations
 import heapq, bisect, math, itertools
 
 #----------------------------------------#
-def dfs(cur, safe, dist):
-    if cur in seen:
-        return
-    seen.add(cur)
-    for nxt in graph[cur]:
-        if nxt not in seen:
-            if not is_safe[nxt]:
-                dists[nxt].add(dist + 1)
-            dfs(nxt, is_safe[nxt], dist + 1)
-    return
-
 n, m = map(int, input().split())
 graph = [[] for _ in range(n)]
 for _ in range(m):
@@ -29,21 +18,22 @@ for _ in range(m):
     graph[v].append(u)
 s = input().strip()
 
-start = []
-is_safe = [False]*n
+q = deque()
 for i in range(n):
     if s[i] == "S":
-        is_safe[i] = True
-        start.append(i)
-    else:
-        is_safe[i] = False
+        q.append((i, i, 0))
 
-dists = [SortedList() for _ in range(n)]
-for node in start:
-    seen = set()
-    dfs(node, True, 0)
+dist = [0]*n
+starts = [set() for _ in range(n)]
+while q:
+    start, cur, d = q.popleft()
+    if start in starts[cur] or len(starts[cur]) >= 2: continue
+    dist[cur] += d
+    starts[cur].add(start)
+    for nxt in graph[cur]:
+        if not start in starts[nxt] and len(starts[nxt]) < 2:
+            q.append((start, nxt, d + 1))
 
 for i in range(n):
-    if not is_safe[i]:
-        print(dists[i][0] + dists[i][1])
-print(dists)
+    if s[i] == "D":
+        print(dist[i])
