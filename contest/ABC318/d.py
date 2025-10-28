8ebd4ad45d2d7b0519d524f2e9348361ac26bbd1
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-10-28 10:21:56 +0900

    ABC318D

--- contest/ABC318/d.py
+++ contest/ABC318/d.py
@@ -14,3 +14,19 @@ p = []
 for i in range(n - 1):
     d = list(map(int, input().split()))
     p.append(d)
+dp = [0]*(1 << n)
+for bit in range((1 << n) - 1):
+    l = -1
+    for i in range(n):
+        if not(bit >> i & 1):
+            l = i
+            break
+    for i in range(l + 1, n):
+        if not(bit >> i & 1):
+            nxt = bit | (1 << l) | (1 << i)
+            dp[nxt] = max(dp[nxt], dp[bit] + p[l][i - l - 1])
+print(dp[-1])
+
+# bitDPを本格的に学ばないといけなくなってきた。
+# 使った頂点の集合をbitで管理し、配るDPのイメージかな
+# ビットシフト絡むと頭爆発するわ。辺の重みの配列もきもいしさあ
