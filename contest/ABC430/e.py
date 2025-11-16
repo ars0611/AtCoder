3476250e40690b9c1eb936c1ffeac10295a210e3
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-11-16 17:39:22 +0900

    solve: ABC430E

--- contest/ABC430/e.py
+++ contest/ABC430/e.py
@@ -9,3 +9,42 @@ from more_itertools import distinct_permutations
 import heapq, bisect, math, itertools
 
 #----------------------------------------#
+# sh = StringHash(s)
+class StringHash:
+    def __init__(self, s, base = 100, mod = 2147483647):
+        self.n = len(s)
+        self.mod = mod
+        self.base = base
+
+        # アルファベットを 0〜25 に変換
+        t = [ord(ch) - ord('a') for ch in s]
+
+        # base**i を前計算
+        self.power = [1] * (self.n + 1)
+        for i in range(self.n):
+            self.power[i + 1] = self.power[i] * self.base % self.mod
+
+        # h[i] := 先頭から i 文字目までのハッシュ値
+        self.h = [0] * (self.n + 1)
+        for i in range(self.n):
+            self.h[i + 1] = (self.h[i] * self.base + t[i]) % self.mod
+
+    # s[l:r](0-index)のハッシュ値
+    def get_hash(self, l, r):
+        return (self.h[r] - self.h[l] * self.power[r - l]) % self.mod
+
+t = int(input())
+for _ in range(t):
+    a = input().strip()
+    b = input().strip()
+    n = len(a)
+    sh_a = StringHash(a + a)
+    sh_b = StringHash(b)
+    hash_b = sh_b.get_hash(0, n)
+    for i in range(n):
+        hash_a = sh_a.get_hash(i, n + i)
+        if hash_a == hash_b:
+            print(i)
+            break
+    else:
+        print(-1)
