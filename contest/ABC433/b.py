e2d6d858ac4812642ca5af80874232e01567aa08
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-11-22 23:15:38 +0900

    solve: ABC433 A, B, C, D

---
+++ contest/ABC433/b.py
@@ -0,0 +1,27 @@
+import sys
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+#----------------------------------------#
+import math
+import bisect
+import itertools
+import heapq
+from collections import deque
+from collections import Counter
+from collections import defaultdict
+from sortedcontainers import SortedList
+from sortedcontainers import SortedSet
+from sortedcontainers import SortedDict
+from more_itertools import distinct_permutations
+#----------------------------------------#
+n = int(input())
+a = list(map(int, input().split()))
+for i in range(n):
+    for j in range(i - 1, -1, -1):
+        if a[i] < a[j]:
+            print(j + 1)
+            break
+    else:
+        print(-1)
