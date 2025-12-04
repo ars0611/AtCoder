3c4f6ab2e5026be3191091d2f71cc2be708f2fef
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-12-04 22:57:40 +0900

    solve: ABC289 D

--- contest/ABC289/d.py
+++ contest/ABC289/d.py
@@ -16,4 +16,16 @@ from sortedcontainers import SortedSet
 from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 #----------------------------------------#
-
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = set(map(int, input().split()))
+x = int(input())
+stairs = [False]*(x + 1)
+stairs[0] = True
+for i in range(x + 1):
+    if not stairs[i]: continue
+    for j in range(n):
+        if i + a[j] <= x and i + a[j] not in b:
+            stairs[i + a[j]] = True
+print("Yes" if stairs[x] else "No")
