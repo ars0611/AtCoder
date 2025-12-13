d8bf3a05c256f05d3ddc214cd8029fbdd7b9628d
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-12-13 17:18:03 +0900

    solve: ABC288 D

--- contest/ABC282/d.py
+++ contest/ABC282/d.py
@@ -16,3 +16,40 @@ from sortedcontainers import SortedSet
 from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 #----------------------------------------#
+def bfs(node):
+    isBipartite = True
+    q = deque([(node, -1)])
+    white = 0
+    black = 0
+    while q:
+        cur, prevColor = q.popleft()
+        if color[cur] != 0: continue
+        color[cur] = -1 * prevColor
+        if color[cur] == 1:
+            white += 1
+        else:
+            black += 1
+        for nxt in graph[cur]:
+            if color[nxt] == color[cur]:
+                isBipartite = False
+            elif color[nxt] == 0:
+                q.append((nxt, color[cur]))
+    return white, black, isBipartite
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    u, v = map(lambda x: int(x) - 1, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+color = [0]*n
+isBipartite = True
+ans = n * (n - 1) // 2 - m
+for i in range(n):
+    if color[i] != 0: continue
+    white, black, isBipartite = bfs(i)
+    if not isBipartite:
+        ans = 0
+        break
+    ans -= black * (black - 1) // 2 + white * (white - 1) // 2
+print(ans)
