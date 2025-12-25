66f578e06d2ccab2544ed259bc0e0148c2bb68a7
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-12-25 12:09:59 +0900

    solve: ABC275 D

--- contest/ABC275/d.py
+++ contest/ABC275/d.py
@@ -16,4 +16,21 @@ from sortedcontainers import SortedSet
 from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 #----------------------------------------#
-
+def f(k):
+    if k == 0:
+        return 1
+    if k // 2 in memo.keys() and k // 3 in memo.keys():
+        res = memo[k // 2] + memo[k // 3]
+        memo[k] = res
+    elif k // 2 in memo.keys():
+        res = memo[k // 2] + f(k // 3)
+    elif k // 3 in memo.keys():
+        res = f(k // 2) + memo[k // 3]
+    else:
+        res = f(k // 2) + f(k // 3)
+    return res
+memo = {0:1}
+n = int(input())
+ans = f(n)
+print(ans)
+# なんかうまくDPしたくなるけどいいや
