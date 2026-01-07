392a83c6d5e607011491cd1c84fde5bd76f42a32
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-01-07 22:34:56 +0900

    solve: ABC267 D

--- contest/ABC267/d.py
+++ contest/ABC267/d.py
@@ -17,18 +17,17 @@ from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 from functools import lru_cache
 #----------------------------------------#
-inf = -2 * 10 ** 5 * 2000
+inf = -2 * 10 ** 5 * 2000 * 2000 - 1
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 dp = [[inf] * (m + 1) for _ in range(n + 1)]
 dp[0][0] = 0
 for i in range(n):
-    for j in range(m):
+    for j in range(m + 1):
         if dp[i][j] == inf: continue
-        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (j + 1) * a[i])
         dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
+        if j < m:
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (j + 1) * a[i])
 print(dp[n][m])
-for i in range(n + 1):
-    print(*dp[i])
 
-# 惜しい気がする
+# infの取り方適当でペナるのはもったいないので上限・下限の見積もりは正しくやろう
