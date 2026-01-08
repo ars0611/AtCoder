41c485455a2148d385f48f607fb8a643445435ec
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-01-08 23:36:11 +0900

    solve: ABC266 D

--- contest/ABC266/d.py
+++ contest/ABC266/d.py
@@ -17,4 +17,25 @@ from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 from functools import lru_cache
 #----------------------------------------#
-
+n = int(input())
+t = [0]
+x = [0]
+a = [0]
+for i in range(n):
+    ti, xi, ai = map(int, input().split())
+    t.append(ti)
+    x.append(xi)
+    a.append(ai)
+dp = [[-1]*5 for _ in range(n + 1)]
+dp[0][0] = 0
+for i in range(n):
+    d = t[i + 1] - t[i]
+    for j in range(5):
+        if dp[i][j] == -1: continue
+        for k in range(5):
+            if abs(j - k) > d: continue
+            if k == x[i + 1]:
+                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + a[i + 1])
+            else:
+                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j])
+print(max(dp[n]))
