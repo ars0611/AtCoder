f8f4308308322b07a0a7d21587ab1754c2a15a63
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-01-11 20:54:19 +0900

    solve:ABC264 C

--- contest/ABC264/c.py
+++ contest/ABC264/c.py
@@ -17,4 +17,25 @@ from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 from functools import lru_cache
 #----------------------------------------#
+h1, w1 = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(h1)]
+h2, w2 = map(int, input().split())
+b = [list(map(int, input().split())) for _ in range(h2)]
 
+flg = False
+for delR in itertools.combinations(list(range(h1)), h1 - h2):
+    for delC in itertools.combinations(list(range(w1)), w1 - w2):
+        delR = set(delR)
+        delC = set(delC)
+        nR = sorted(list(set(range(h1)) - delR))
+        nC = sorted(list(set(range(w1)) - delC))
+        dr = {nR[i]:i for i in range(h2)}
+        dc = {nC[i]:i for i in range(w2)}
+        grid = [[None]*w2 for _ in range(h2)]
+        for r in range(h1):
+            if r in delR: continue
+            for c in range(w1):
+                if c in delC: continue
+                grid[dr[r]][dc[c]] = a[r][c]
+        flg = True if all(all(grid[i][j] == b[i][j] for j in range(w2)) for i in range(h2)) else flg
+print("Yes" if flg else "No")
