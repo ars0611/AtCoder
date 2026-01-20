507243a860d65a9aa6323adc69e4fadb9dd09ceb
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-01-20 12:09:32 +0900

    solve: ABC256 D

--- contest/ABC256/d.py
+++ contest/ABC256/d.py
@@ -17,4 +17,15 @@ from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 from functools import lru_cache
 #----------------------------------------#
-
+n = int(input())
+seg = sorted(list(tuple(map(int, input().split())) for _ in range(n)))
+curLeft, curRight = seg[0]
+for i in range(n + 1):
+    if i < n:
+        li, ri = seg[i]
+    if i == n or curRight < li:
+        print(curLeft, curRight)
+        curLeft = li
+        curRight = ri
+    else:
+        curRight = max(curRight, ri)
