ffea80814b60a49804675b53a080ea24251a980f
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-01-31 16:25:34 +0900

    solve: ABC442 E

--- contest/ABC442/e.py
+++ contest/ABC442/e.py
@@ -16,76 +16,44 @@ from sortedcontainers import SortedSet
 from sortedcontainers import SortedDict
 from more_itertools import distinct_permutations
 from functools import lru_cache
+from functools import cmp_to_key
 #----------------------------------------#
-n, q = map(int, input().split())
-mon = []
-for i in range(n):
-    x, y = map(int, input().split())
-    if x == 0:
-        if y > 0:
-            inc = 10 ** 10 * 1.0
-        else:
-            inc = -10 ** 10 * 1.0
-    else:
-        inc = y / x
-    g = math.gcd(abs(x), abs(y))
-    mon.append((inc, y, x, i, g))
-mon.sort(reverse=True)
-query = [tuple(map(lambda x:int(x) - 1, input().split())) for _ in range(q)]
-zipped = dict()
-preSum = [0]
-incs = []
-idx = 0
-for i in range(n):
-    inc, y, x, num, g = mon[i]
-    if inc >= 0.0 and y >= 0 and x >= 0:
-        if incs and incs[-1][0] == inc and x / g == incs[-1][1] and y / g == incs[-1][2]:
-            zipped[num] = idx - 1
-            preSum[-1] += 1
-        else:
-            zipped[num] = idx
-            preSum.append(preSum[-1] + 1)
-            idx += 1
-        incs.append((inc, x / g, y / g))
-for i in range(n):
-    inc, y, x, num, g = mon[i]
-    if inc < 0.0 and y < 0 and x >= 0:
-        if incs and incs[-1][0] == inc and x / g == incs[-1][1] and y / g == incs[-1][2]:
-            zipped[num] = idx - 1
-            preSum[-1] += 1
-        else:
-            zipped[num] = idx
-            preSum.append(preSum[-1] + 1)
-            idx += 1
-        incs.append((inc, x / g, y / g))
-for i in range(n):
-    inc, y, x, num, g = mon[i]
-    if inc > 0.0 and y < 0 and x <= 0:
-        if incs and incs[-1][0] == inc and x / g == incs[-1][1] and y / g == incs[-1][2]:
-            zipped[num] = idx - 1
-            preSum[-1] += 1
-        else:
-            zipped[num] = idx
-            preSum.append(preSum[-1] + 1)
-            idx += 1
-        incs.append((inc, x / g, y / g))
-for i in range(n):
-    inc, y, x, num, g = mon[i]
-    if inc <= 0.0 and y >= 0 and x <= 0:
-        if incs and incs[-1][0] == inc and x / g == incs[-1][1] and y / g == incs[-1][2]:
-            zipped[num] = idx - 1
-            preSum[-1] += 1
-        else:
-            zipped[num] = idx
-            preSum.append(preSum[-1] + 1)
-            idx += 1
-        incs.append((inc, x / g, y / g))
-for a, b in query:
-    idxA = zipped[a]
-    idxB = zipped[b]
-    if idxB >= idxA:
-        ans = preSum[idxB + 1] - preSum[idxA]
-    else:
-        ans = preSum[-1] - preSum[idxA] + preSum[idxB + 1]
-    print(ans)
+# 90度から時計回りにソート
+def half(p):
+    x, y = p
+    return 0 if (y > 0 or (y == 0 and x > 0)) else 1
+def declinationSort(a, b):
+    ha = half(a)
+    hb = half(b)
+    if ha != hb:
+        return ha - hb
+    ax, ay = a
+    bx, by = b
+    return ax * by - ay * bx
+keyFunc = cmp_to_key(declinationSort)
 
+n, q = map(int, input().split())
+monsters = [(tuple(map(int, input().split()))) for _ in range(n)]
+pos = {i:monsters[i] for i in range(n)}
+query = [tuple(map(lambda x:int(x) - 1, input().split())) for _ in range(q)]
+monsters.sort(key=keyFunc)
+zipped = {monsters[0]:1}
+preSum = [0, 1]
+cur = 1
+for i in range(1, n):
+    if declinationSort(monsters[i - 1], monsters[i]) == 0:
+        preSum[-1] += 1
+    else:
+        preSum.append(preSum[-1] + 1)
+        cur += 1
+    zipped[monsters[i]] = cur
+ans = []
+for a, b in query:
+    if declinationSort(pos[a], pos[b]) > 0:
+        res = preSum[-1] - preSum[zipped[pos[a]] - 1] + preSum[zipped[pos[b]]]
+    else:
+        res = preSum[zipped[pos[b]]] - preSum[zipped[pos[a]] - 1]
+    ans.append(str(res))
+print("\n".join(ans))
+
+# ToDo: 偏角ソートライブラリ化
