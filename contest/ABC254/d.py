1c05d0da725e14b7178beabd22b86d4912e7a66e
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2026-02-01 23:25:23 +0900

    solve: ABC254 D

--- contest/ABC254/d.py
+++ contest/ABC254/d.py
@@ -18,4 +18,27 @@ from more_itertools import distinct_permutations
 from functools import lru_cache
 from functools import cmp_to_key
 #----------------------------------------#
+def getMaxSqDivisor(k):
+    rootK = math.isqrt(k)
+    res = 1
+    for div in range(1, rootK + 1):
+        if k % div == 0:
+            if k // div in sq:
+                res = max(res, k // div)
+            if div in sq:
+                res = max(res, div)
+    return res
 n = int(input())
+sq = set()
+for i in range(1, n + 1):
+    sq.add(i * i)
+counter = Counter()
+for i in range(1, n + 1):
+    counter[i // getMaxSqDivisor(i)] += 1
+ans = 0
+for i in range(1, n + 1):
+    ans += counter[i // getMaxSqDivisor(i)]
+print(ans)
+# 数学すぎてわからん
+# ある整数iを素因数分解して指数の偶奇で分けて考えると i = iを割り切れる最大の平方数 * 指数が奇数の素因数の積 となる
+# i * jが平方数になるのは j = i / iを割り切れる最大の平方数 * 平方数となる。
