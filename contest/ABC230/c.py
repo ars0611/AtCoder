075c0a8833e2a50afbf88599f3b8d3b9a9f26836
ars0611 <yonehei@outlook.jp>
2026-04-09 12:20:52 +0900

    solve: ABC230 C

--- contest/ABC230/c.py
+++ contest/ABC230/c.py
@@ -21,19 +21,20 @@ from functools import cmp_to_key
 n, a, b = map(int, input().split())
 p, q, r, s = map(int, input().split())
 bs = (a + max(1 - a, 1 - b), b + max(1 - a, 1 - b))
-be = (b + min(n - a, n - b), b - min(n - a, n - b))
-bbs = (a + max(1 - a, b - n), b - max(1 - 1, b - n))
+be = (a + min(n - a, n - b), b + min(n - a, n - b))
+bbs = (a + max(1 - a, b - n), b - max(1 - a, b - n))
 bbe = (a + min(n - a, b - 1), b - min(n - a, b - 1))
 
 ans = [['.']*(s - r + 1) for _ in range(q - p + 1)]
-for i in range(s - r + 1):
-    for j in range(q - p + 1):
-        if bs[0] <= i + 1 <= be[0] and bs[1] <= j + 1 <= be[1]:
-            if
+for i in range(q - p + 1):
+    for j in range(s - r + 1):
+        curi = p + i
+        curj = r + j
+        if bs[0] <= curi <= be[0] and bs[1] <= curj <= be[1]:
+            if curi - bs[0] == curj - bs[1]:
                 ans[i][j] = '#'
-        if bbs[0] <= i + 1 <= bbe[0] and bbs[1] <= j + 1 <= bbe[1]:
-            if 
+        if bbs[0] <= curi <= bbe[0] and bbe[1] <= curj <= bbs[1]:
+            if curi - bbs[0] == bbs[1] - curj:
                 ans[i][j] = '#'
-for i in range(s - r + 1):
+for i in range(q - p + 1):
     print(''.join(ans[i]))
-#後で解く
