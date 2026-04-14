7d427a6ba0034e1d203acdcab6963490fa8064c7
ars0611 <yonehei@outlook.jp>
2026-04-14 09:43:29 +0900

    solve: ABC229 D

--- contest/ABC229/d.py
+++ contest/ABC229/d.py
@@ -20,6 +20,22 @@ from functools import cmp_to_key
 #----------------------------------------#
 s = input().strip()
 k = int(input())
+n = len(s)
+ans = 0
+cnt = 0
+r = 0
+for l in range(n):
+    r = max(r, l)
+    if l > 0 and s[l - 1] == '.':
+        cnt = max(cnt - 1, 0)
+    while r < n and cnt <= k:
+        if cnt == k and s[r] == '.':
+            break
+        if s[r] == '.':
+            cnt += 1
+        r += 1
+    ans = max(ans, r - l)
+print(ans)
 
 # .をk個以下しか含まないようなsubstringの長さ
 # 尺取っぽくやりたい
