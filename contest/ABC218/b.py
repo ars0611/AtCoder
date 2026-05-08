d03494e75e9d46188464eaac070d92b73183fe03
ars0611 <yonehei@outlook.jp>
2026-05-08 15:05:32 +0900

    solve: ABC218 A, B

---
+++ contest/ABC218/b.py
@@ -0,0 +1,23 @@
+import sys
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+#----------------------------------------#
+import math
+import bisect
+import itertools
+import heapq
+from collections import deque
+from collections import Counter
+from collections import defaultdict
+from sortedcontainers import SortedList
+from sortedcontainers import SortedSet
+from sortedcontainers import SortedDict
+from more_itertools import distinct_permutations
+from functools import lru_cache
+from functools import cmp_to_key
+#----------------------------------------#
+p = list(map(int, input().split()))
+s = [chr(ord('a') + p[i] - 1) for i in range(26)]
+print(''.join(s))
